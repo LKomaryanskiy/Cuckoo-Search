35472cb303a37d84d7195d263678d626f37c1a2d
Leonid <lkomaryanskiy@gmail.com>
2017-02-16 08:39:56 +0200

    Initial commit, added primary logic

--- Cuckoo search/Test.cpp
+++ Cuckoo search/Test.cpp
@@ -1,10 +1,7 @@
-#include "LevyFlight.h"
+#include "CuckooSearch.h"
 #include <stdlib.h>
 
 int main()
 {
-	for (int i = 0; i < 10; ++i) {
-		std::valarray<double> test = LevyFlight::GetValue(10);
-	}
 	return EXIT_SUCCESS;
 }
